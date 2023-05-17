5fa16985e88bdae47ffe15efdb3700a230916533
Mansur <mansuraitqul@gmail.com>
2023-05-17 20:22:51 +0600

    Update

---
+++ resume/study/base/data_types.py
@@ -0,0 +1,21 @@
+dict3 = {"name": "Pyhton"}
+int10 = 12.0
+print(type(int10))
+print(type(dict3), dict3)
+
+INT_CONSTANT = 12
+print(INT_CONSTANT)
+print(INT_CONSTANT / 9)
+print(INT_CONSTANT // 9)
+
+bool1 = True
+bool2 = False
+
+
+print(bool1) #True
+type_bool1 = type(bool1)
+
+print(isinstance(bool1, str))
+print(isinstance(bool1, bool))
+
+source_str1 = "Python1! is awesome"
